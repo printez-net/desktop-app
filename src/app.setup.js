a42305a66e94cb5ee919246948e7fcaa70d96e40
Tu Tran <tutv95@gmail.com>
2023-04-14 00:38:39 +0700

    udapted

--- src/app.setup.js
+++ src/app.setup.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 
 module.exports = () => {
+    app.setName('PrintEZ')
+
     const iconPath = path.join(__dirname, '../assets/images/logo.png')
     app.dock.setIcon(iconPath)
 }
