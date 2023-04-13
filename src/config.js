0517a4dd2026feb0f70f48da4c8f87357399470e
Tu Tran <tutv95@gmail.com>
2023-04-14 00:02:35 +0700

    udapted

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const fileName = process.env.NODE_ENV === 'production' ? 'production.env' : '.env'
+
 require('dotenv').config({
-    path: require('path').resolve(__dirname, '../.env')
+    path: require('path').resolve(__dirname, '..', fileName)
 })
