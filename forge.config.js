7d044d5f71b1536cc64f071c18ad1d0f7c6e38ee
Tu Tran <tutv95@gmail.com>
2023-04-14 22:49:27 +0700

    udpated

--- forge.config.js
+++ forge.config.js
@@ -6,19 +6,14 @@ module.exports = {
     makers: [
         {
             name: '@electron-forge/maker-squirrel',
-            config: {},
+            config: {
+                authors: 'Printez',
+                description: 'Printez Desktop App',
+            },
         },
         {
             name: '@electron-forge/maker-zip',
             platforms: ['darwin'],
-        },
-        {
-            name: '@electron-forge/maker-deb',
-            config: {},
-        },
-        {
-            name: '@electron-forge/maker-rpm',
-            config: {},
         },
     ],
     publishers: [
